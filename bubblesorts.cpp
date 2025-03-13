095f5be0a411317c4313694b2c0019fc81d6a6d4
restuchan <restu.chan188@gmail.com>
2025-03-13 11:14:31 +0700

    memasukkan void display

--- bubblesorts.cpp
+++ bubblesorts.cpp
@@ -42,3 +42,17 @@ void  bubbleSortArray(){
         pass = pass + 1; //step 4
     }while(pass <= n-1);  //step 5
 }
+
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n=1) {
+            cout << " --> ";
+        }
+    }
+}
